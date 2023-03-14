dd7004260cb07925ba2ddd7e13ccff277dd5240d
Code 41 <washingtonbenton@gmail.com>
2023-03-14 13:10:18 -0400

    finish expense tracker and add chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,19 +1,22 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
-
 
 
 function Expenses(props) {
     const [filteredYear, setFilteredYear] = useState('2020');
 
-
     function filterChangeHandler(selectedYear) {
         setFilteredYear(selectedYear);
     };
+
+    const filteredExpenses = props.items.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
 
 
     return (
@@ -23,18 +26,25 @@ function Expenses(props) {
                 selected={filteredYear} 
                 onChangeFilter={filterChangeHandler}
             />
-
-            {props.items.map((expense) => (
-                <ExpenseItem 
-                    key={expense.id}
-                    title={expense.title} 
-                    amount={expense.amount}  
-                    date={expense.date}
-                />
-                    ))}
+            <ExpensesChart expenses={filteredExpenses}/>
+            <ExpensesList items={filteredExpenses} />
         </Card>
         </div>
     );
 };
 
 export default Expenses;
+
+
+            {/* {filteredExpenses.length === 0 ? (
+                <p>No expenses found.</p>
+            ) : (
+                filteredExpenses.map((expense) => (
+                    <ExpenseItem 
+                        key={expense.id}
+                        title={expense.title} 
+                        amount={expense.amount}  
+                        date={expense.date}
+                    />
+                        ))
+            )} */}
